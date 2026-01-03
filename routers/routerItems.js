e038cfbe705c40f8939c142608d1d2fd23647f7e
Daniel Onís Fabián <danielonisfabian@MacBook-Air-de-Daniel-244.local>
2026-01-03 16:25:52 +0100

    routerItems GET endpoints

---
+++ routers/routerItems.js
@@ -0,0 +1,22 @@
+const express = require("express")
+
+let routerItems = express.Router()
+
+let items = require("../data/items")
+
+routerItems.get("/", (req,res) => {
+    res.json(items)
+})
+
+routerItems.get("/:id", (req,res) => {
+    let item = items.find( i => i.id == req.params.id)
+
+    if(item == undefined){
+        res.status(400).json({error: "no item with this id"})
+        return
+    }
+
+    res.json(item)
+})
+
+module.exports = routerItems
